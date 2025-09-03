d7753f9790149f12291ba714fb3bbcde766a4202
geovannifrnaca <geovannidefranca98@gmail.com>
2025-09-03 16:59:09 -0300

    ajustado

---
+++ lib/ui/colors/app_colors.dart
@@ -0,0 +1,10 @@
+
+import 'package:flutter/material.dart';
+
+class AppColors{
+  static const Color blue = Color(0xFF3698D4);
+  static const Color white = Color(0xFFF8F8F8);
+  static const Color bgWhite = Color(0xFFEAEAEA);
+  static const Color fontGrayBlack = Color(0xFF404040);
+  static const Color fontGrayLight = Color(0xFFC0C0C0);
+}
